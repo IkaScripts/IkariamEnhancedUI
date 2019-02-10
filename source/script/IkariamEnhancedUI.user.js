361219a7c1649ac7dabeec849905b2a8719609d1
Tobbe <contact@ikascripts.de>
2019-02-10 11:43:56 +0100

    Update language handling to have all translations in the script.

--- source/script/IkariamEnhancedUI.user.js
+++ source/script/IkariamEnhancedUI.user.js
@@ -19,8 +19,6 @@
 // @require			//@REQUIRE_IKARIAM_CORE@//
 // 
 // @connect			greasyfork.org
-//
-// //@RESOURCE_LANGUAGE_INCLUDES@//
 // 
 // @grant			unsafeWindow
 // @grant			GM_setValue
@@ -42,7 +40,8 @@
 // @bug				All				If you are zooming to more than 100%, the view is not centered correctly after a page reload.
 // 
 // @history			next	Release: ???
-// @history			next	Core: Update to Version 3.1.0.2 - Greasy Fork prohibits update check intervals smaller than 24 hours.
+// @history			next	Core: Update to Version 3.1.0.2 - Greasy Fork prohibits update check intervals smaller than 24 hours
+// @history			next	Language: Include all translations in the script and remove external dependencies for translations
 // 
 // @history			4.1.0.1	Release: 14.11.2017
 // @history			4.1.0.1	Core: Update to Version 3.1.0.1 - Fix issue with Greasemonkey 4.0 and incorrect link to translation files
@@ -350,15 +349,13 @@ async function main() {
 	
 	await IC.Language.setDefaultLanguage('en');
 	
-	await IC.Language.addLanguageText('en', //@SCRIPT_LANGUAGE_DEFAULT@//);
-	
-	var la_language = //@SCRIPT_TRANSLATIONS_ARRAY@//;
+	var la_language = //@SCRIPT_TRANSLATIONS_DATA@//;
 	for(var i = 0; i < la_language.length; i++) {
-		await IC.Language.registerLanguageResource(la_language[i], la_language[i], '//@RESOURCE_LANGUAGE_URL@///' + la_language[i] + '.json');
+		await IC.Language.addLanguageText(la_language[i].code, la_language[i].translations);
 	}
 	
 	// Instantiate the ui script.
-	new EnhancedUI(IC);
+	setTimeout(function() { new EnhancedUI(IC); }, 100);
 }
 
 // Call the main function of the script.
